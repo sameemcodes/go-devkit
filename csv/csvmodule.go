019b36af2d278ab7182e874ae2624e61baf906ee
sameemcodes <mmmohamedsameem@gmail.com>
2023-11-10 20:11:46 +0530

    csv module

--- csv/csvmodule.go
+++ csv/csvmodule.go
@@ -2,10 +2,10 @@
 package csvmodule
 
 import (
-    "encoding/csv"
-    "os"
-    "sync"
-	"io" 
+	"encoding/csv"
+	"io"
+	"os"
+	"sync"
 )
 
 // ReadCSV reads a CSV file concurrently
@@ -56,47 +56,81 @@ func ReadCSV(filename string, maxGoroutines int) ([][]string, error) {
 		return nil, err
 	}
 }
+
 // WriteCSV writes data to a CSV file concurrently
 func WriteCSV(data [][]string, filename string, maxGoroutines int) error {
-    file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-    if err != nil {
-        return err
-    }
-    defer file.Close()
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
 
-    writer := csv.NewWriter(file)
-    defer writer.Flush()
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
 
-    var wg sync.WaitGroup
-    goroutineSem := make(chan struct{}, maxGoroutines)
+	var wg sync.WaitGroup
+	goroutineSem := make(chan struct{}, maxGoroutines)
 
-    errors := make(chan error, 1)
-    done := make(chan bool, 1)
+	errors := make(chan error, 1)
+	done := make(chan bool, 1)
 
-    go func() {
-        for _, row := range data {
-            goroutineSem <- struct{}{}
-            wg.Add(1)
+	go func() {
+		for _, row := range data {
+			goroutineSem <- struct{}{}
+			wg.Add(1)
 
-            go func(row []string) {
-                defer wg.Done()
-                if err := writer.Write(row); err != nil {
-                    select {
-                    case errors <- err:
-                    default:
-                    }
-                }
-                <-goroutineSem
-            }(row)
-        }
-        wg.Wait()
-        done <- true
-    }()
+			go func(row []string) {
+				defer wg.Done()
+				if err := writer.Write(row); err != nil {
+					select {
+					case errors <- err:
+					default:
+					}
+				}
+				<-goroutineSem
+			}(row)
+		}
+		wg.Wait()
+		done <- true
+	}()
 
-    select {
-    case <-done:
-        return nil
-    case err := <-errors:
-        return err
-    }
+	select {
+	case <-done:
+		return nil
+	case err := <-errors:
+		return err
+	}
 }
+
+// AppendCSVSequentially appends data to a CSV file sequentially using multiple goroutines
+func AppendCSVSequentially(data [][]string, filename string, maxGoroutines int) error {
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	var wg sync.WaitGroup
+
+	for _, row := range data {
+		wg.Add(1)
+
+		go func(row []string) {
+			defer wg.Done()
+			if err := writer.Write(row); err != nil {
+				panic(err)
+			}
+		}(row)
+	}
+
+	wg.Wait()
+
+	return nil
+}
+
+
+
+	
